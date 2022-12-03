680d8fd1cc89906c9b5d067168fad6c6788aa5cd
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-03 01:36:53 +0100

    added eventListener for color change

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { Marker } from "./Marker.js";
 import { rotateKnob } from "./rotateKnob.js";
 import { SetMarkerSize } from "./SetMarkerSize.js";
+import { setMarkerColor } from "./setMarkerColor.js";
 const marker = new Marker();
 main()
 
@@ -15,7 +16,11 @@ function addKeyboardListeners() {
         marker.move(event.key);
     });
 
-    document.querySelector("input").addEventListener("change", (event) => {
+    document.querySelector(".number").addEventListener("change", (event) => {
         SetMarkerSize(event, marker);
     });
+
+    document.querySelector(".color").addEventListener("change" , (event) => {
+        setMarkerColor(event, marker);
+    })
 }
